625a83dc3f92bdbde0385be85f5dc9d7048d66e3
Fola <folaoso23@gmail.com>
2020-12-16 15:21:15 -0500

    Updated setup shell

--- pipelines/deploy-pipeline/setup.sh
+++ pipelines/deploy-pipeline/setup.sh
@@ -8,6 +8,10 @@ oc apply -f helm-deploy-pipeline.yaml
 # Crete a DEV namespace for the application
  oc new-project springboot-camel-example-dev
  oc adm policy add-role-to-user -n springboot-camel-example-dev edit: system:serviceaccount:$CURRENT_NAMESPACE:deploy_bot
+ oc adm policy add-role-to-user -n springboot-camel-example-dev edit: system:serviceaccount:$CURRENT_NAMESPACE:pipeline
+
+ # TODO: parametrize nexus registry URL
+ oc create secret docker-registry nexus-credentials --docker-server=nexus-container-registry-nexus.apps.cluster-3bcb.sandbox1869.opentlc.com --docker-username=admin --docker-password=admin123
 
  oc project $CURRENT_NAMESPACE
  
