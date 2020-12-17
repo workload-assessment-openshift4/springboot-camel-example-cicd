c02dc18f34310ea2fa42f63faa813d43edce7854
Szeti, balazs <bszeti@mtahq.org>
2020-12-17 16:25:24 +0100

    fix

--- pipelines/deploy-pipeline/setup.sh
+++ pipelines/deploy-pipeline/setup.sh
@@ -6,16 +6,14 @@ oc apply -f helm-deploy-pipeline.yaml
 
 
 # Crete a DEV namespace for the application
- oc new-project springboot-camel-example-dev
- oc adm policy add-role-to-user -n springboot-camel-example-dev edit system:serviceaccount:$CURRENT_NAMESPACE:deploy_bot
- oc adm policy add-role-to-user -n springboot-camel-example-dev edit system:serviceaccount:$CURRENT_NAMESPACE:pipeline
- oc create secret docker-registry nexus-credentials --docker-server=nexus-container-registry-nexus.apps.cluster-3bcb.sandbox1869.opentlc.com --docker-username=admin --docker-password=admin123
+oc new-project springboot-camel-example-dev
+oc adm policy add-role-to-user -n springboot-camel-example-dev edit system:serviceaccount:$CURRENT_NAMESPACE:deploy-bot
+oc create secret docker-registry nexus-credentials --docker-server=nexus-container-registry-nexus.apps.cluster-3bcb.sandbox1869.opentlc.com --docker-username=admin --docker-password=admin123
 
- # Crete a QA namespace for the application
- oc new-project springboot-camel-example-qa
- oc adm policy add-role-to-user -n springboot-camel-example-qa edit system:serviceaccount:$CURRENT_NAMESPACE:deploy_bot
- oc adm policy add-role-to-user -n springboot-camel-example-qa edit system:serviceaccount:$CURRENT_NAMESPACE:pipeline
- oc create secret docker-registry nexus-credentials --docker-server=nexus-container-registry-nexus.apps.cluster-3bcb.sandbox1869.opentlc.com --docker-username=admin --docker-password=admin123
+# Crete a QA namespace for the application
+oc new-project springboot-camel-example-qa
+oc adm policy add-role-to-user -n springboot-camel-example-qa edit system:serviceaccount:$CURRENT_NAMESPACE:deploy-bot
+oc create secret docker-registry nexus-credentials --docker-server=nexus-container-registry-nexus.apps.cluster-3bcb.sandbox1869.opentlc.com --docker-username=admin --docker-password=admin123
 
- oc project $CURRENT_NAMESPACE
+oc project $CURRENT_NAMESPACE
  
